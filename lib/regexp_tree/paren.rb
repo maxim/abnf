9e5532f8073aa8b02a44e504866161aaf0727f9c
Max Chernyak <max@bitsonnet.com>
2022-03-10 16:35:33 -0500

    Clarify ivar name in RegexpTree::Paren

--- lib/regexp_tree/paren.rb
+++ lib/regexp_tree/paren.rb
@@ -4,27 +4,27 @@ class RegexpTree
   # (?ixm-ixm:...)
   # (?>...)
   class Paren < Elt
-    def initialize(r, mark='?:')
+    def initialize(tree, mark='?:')
       @mark = mark
-      @r = r
+      @tree = tree
     end
 
     def case_insensitive?
       # xxx: if @mark contains "i"...
-      @r.case_insensitive?
+      @tree.case_insensitive?
     end
 
     def multiline_insensitive?
       # xxx: if @mark contains "m"...
-      @r.multiline_insensitive?
+      @tree.multiline_insensitive?
     end
 
     def downcase
-      Paren.new(@r.downcase, @mark)
+      Paren.new(@tree.downcase, @mark)
     end
 
     def pretty_format(out)
-      out.group(1 + @mark.length, "(#@mark", ')') { @r.pretty_format(out) }
+      out.group(1 + @mark.length, "(#@mark", ')') { @tree.pretty_format(out) }
     end
   end
 end
