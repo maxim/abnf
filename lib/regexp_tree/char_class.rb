406ec204ed9761f3dd2be673e1c6447576bc85ac
Max Chernyak <max@bitsonnet.com>
2022-03-10 15:57:05 -0500

    Fix indents in RegexpTree::CharClass

--- lib/regexp_tree/char_class.rb
+++ lib/regexp_tree/char_class.rb
@@ -3,24 +3,24 @@ require 'regexp_tree/elt'
 
 class RegexpTree
   class CharClass < Elt
-    None = Natset.empty
-    Any = Natset.universal
-    NL = Natset[?\n]
-    NonNL = ~NL
-    Word = Natset[?0..?9, ?A..?Z, ?_, ?a..?z]
-    NonWord = ~Word
-    Space = Natset[?t, ?\n, ?\f, ?\r, ?\s]
+    None     = Natset.empty
+    Any      = Natset.universal
+    NL       = Natset[?\n]
+    NonNL    = ~NL
+    Word     = Natset[?0..?9, ?A..?Z, ?_, ?a..?z]
+    NonWord  = ~Word
+    Space    = Natset[?t, ?\n, ?\f, ?\r, ?\s]
     NonSpace = ~Space
-    Digit = Natset[?0..?9]
+    Digit    = Natset[?0..?9]
     NonDigit = ~Digit
-
-    UpAlpha = Natset[?A..?Z]
+    UpAlpha  = Natset[?A..?Z]
     LowAlpha = Natset[?a..?z]
+
+    attr_reader :natset
 
     def initialize(natset)
       @natset = natset
     end
-    attr_reader :natset
 
     def empty_set?
       @natset.empty?
@@ -46,15 +46,15 @@ class RegexpTree
 
     def pretty_format(out)
       case @natset
-      when None; out.text '(?!)'
-      when Any; out.text '[\s\S]'
-      when NL; out.text '\n'
-      when NonNL; out.text '.'
-      when Word; out.text '\w'
-      when NonWord; out.text '\W'
-      when Space; out.text '\s'
+      when None;     out.text '(?!)'
+      when Any;      out.text '[\s\S]'
+      when NL;       out.text '\n'
+      when NonNL;    out.text '.'
+      when Word;     out.text '\w'
+      when NonWord;  out.text '\W'
+      when Space;    out.text '\s'
       when NonSpace; out.text '\S'
-      when Digit; out.text '\d'
+      when Digit;    out.text '\d'
       when NonDigit; out.text '\D'
       else
         if val = @natset.singleton?
@@ -62,14 +62,14 @@ class RegexpTree
         else
           neg_mark = @natset.open? ? '^' : ''
 
-          r = ''
-          @natset.ranges.each do |range|
-            r << encode_elt(range.begin)
-            r << '-' if range.size > 2
-            r << encode_elt(range.end)
-          end
+          regex_char_class =
+            @natset.ranges.each_with_object('') do |r|
+              r << encode_elt(range.begin)
+              r << '-' if range.size > 2
+              r << encode_elt(range.end)
+            end
 
-          out.text "[#{neg_mark}#{r}]"
+          out.text "[#{neg_mark}#{regex_char_class}]"
         end
       end
     end
@@ -83,8 +83,11 @@ class RegexpTree
       when 0x0b; '\v'
       when 0x07; '\a'
       when 0x1b; '\e'
-      #when ?!, ?", ?%, ?&, ?', ?,, ?:, ?;, ?<, ?=, ?>, ?/, ?0..?9, ?@, ?A..?Z, ?_, ?`, ?a..?z, ?~
-      when 0x21, 0x22, 0x25, 0x26, 0x27, 0x2c, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x2f, 0x30..0x39, 0x40, 0x41..0x5a, 0x5f, 0x60, 0x61..0x7a, 0x7e
+           # ?!,   ?",   ?%,   ?&,   ?',   ?,,   ?:,   ?;,   ?<,   ?=,   ?>,
+      when 0x21, 0x22, 0x25, 0x26, 0x27, 0x2c, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e,
+           # ?/,   ?0..?9,     ?@,   ?A..?Z,     ?_,   ?`,   ?a..?z,     ?~
+           0x2f, 0x30..0x39, 0x40, 0x41..0x5a, 0x5f, 0x60, 0x61..0x7a, 0x7e
+
         sprintf("%c", e)
       else
         sprintf("\\x%02x", e)
